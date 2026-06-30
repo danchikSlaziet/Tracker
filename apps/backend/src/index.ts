f7f6967c4c602b12f45b184fd85c94a6990facae
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2026-06-30 21:58:48 +0500

    chore: configs for VPS deployment

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.json()) // Чтобы Express умел читать JSON из re
 app.use(cookieParser()) // Чтобы Express умел читать куки из req.cookies
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true, // передача httpOnly куки
   })
 )
