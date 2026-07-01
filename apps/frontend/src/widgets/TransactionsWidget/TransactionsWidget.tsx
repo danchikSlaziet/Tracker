5ca3ce89a21ad27b7c150cd4d23dd83fb8857cd7
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2026-07-01 20:16:47 +0500

    refactor: business logic from pages to widgets

---
+++ apps/frontend/src/widgets/TransactionsWidget/TransactionsWidget.tsx
@@ -0,0 +1,33 @@
+import { CreateTransactionForm } from "@/features/transactions/ui/CreateTransactionForm"
+import { TransactionFiltersPanel } from "@/features/transactions/ui/TransactionFilters/TransactionFilters"
+import { TransactionList } from "@/features/transactions/ui/TransactionList/TransactionList"
+
+import styles from './TransactionsWidget.module.css'
+import { useState } from "react"
+import type { TransactionFilters } from "@/features/transactions/model/transactionsSchema"
+import { useDebounce } from "@/shared/lib/useDebounce"
+
+export const TransactionWidget = () => {
+  const [filters, setFilters] = useState<TransactionFilters>({})
+
+  const debouncedSearch = useDebounce(filters?.search, 400)
+
+  const finalFilters = {
+    ...filters,
+    search: debouncedSearch
+  }
+  return (
+    <>
+      <TransactionFiltersPanel filters={filters} onChange={setFilters} />
+      <div className={styles.content}>
+        <div className={styles.formWrapper}>
+          <CreateTransactionForm />
+        </div>
+        <div className={styles.listWrapper}>
+          <TransactionList filters={finalFilters} />
+        </div>
+      </div>
+    </>
+
+  )
+}
